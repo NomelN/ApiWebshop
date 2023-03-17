58c33f9f5d1326f4cb0d4a48a51a0654484559c4
NomelN <akpanomel@gmail.com>
2023-03-17 01:49:31 +0100

    ajout des tests unitaires

---
+++ ApiWebshop/Tests/AuthentificationControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using ApiWebshop.Controllers;
+using ApiWebshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Security.Claims;
+using Xunit;
+
+namespace ApiWebshop.Tests
+{
+    public class AuthentificationControllerTests
+    {
+        private readonly Mock<InterfaceJwtAuthentificationService> _mockAuthService;
+        private readonly IConfiguration _mockConfig;
+
+        public AuthentificationControllerTests()
+        {
+            _mockAuthService = new Mock<InterfaceJwtAuthentificationService>();
+            _mockConfig = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "Jwt:Key", "mysecretkey" }
+            }).Build();
+        }
+
+        [Fact]
+        public void Login_ReturnsOk_WithValidCredentials()
+        {
+            // Arrange
+            var controller = new AuthentificationController(_mockAuthService.Object, _mockConfig);
+            var model = new ConnexionModel { Email = "admin@gmail.com", Password = "Epsi2023" };
+            var claims = new List<Claim> { new Claim(ClaimTypes.Email, model.Email) };
+            var token = "myjwttoken";
+            _mockAuthService.Setup(s => s.Authenticate(model.Email, model.Password)).Returns(new Utilisateur { Email = model.Email });
+            _mockAuthService.Setup(s => s.GenerateToken(_mockConfig["Jwt:Key"], claims)).Returns(token);
+
+            // Act
+            var result = controller.Login(model);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var actualToken = Assert.IsType<string>(okResult.Value);
+            Assert.Equal(token, actualToken);
+        }
+
+        [Fact]
+        public void Login_ReturnsUnauthorized_WithInvalidCredentials()
+        {
+            // Arrange
+            var controller = new AuthentificationController(_mockAuthService.Object, _mockConfig);
+            var model = new ConnexionModel { Email = "invalid@gmail.com", Password = "invalidpassword" };
+            _mockAuthService.Setup(s => s.Authenticate(model.Email, model.Password)).Returns((Utilisateur)null);
+
+            // Act
+            var result = controller.Login(model);
+
+            // Assert
+            Assert.IsType<UnauthorizedResult>(result);
+        }
+    }
+}
---
+++ ApiWebshop/Tests/JwtAuthentificationServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using ApiWebshop.Models;
+using System.Security.Claims;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApiWebshop.Tests
+{
+    [TestClass]
+    public class JwtAuthentificationServiceTests
+    {
+        private JwtAuthentificationService _jwtAuthentificationService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Arrange
+            _jwtAuthentificationService = new JwtAuthentificationService();
+        }
+
+        [TestMethod]
+        public void Authenticate_ValidCredentials_ReturnsUser()
+        {
+            // Act
+            Utilisateur user = _jwtAuthentificationService.Authenticate("admin@gmail.com", "Epsi2023");
+
+            // Assert
+            Assert.IsNotNull(user);
+            Assert.AreEqual("admin@gmail.com", user.Email);
+        }
+
+        [TestMethod]
+        public void Authenticate_InvalidCredentials_ReturnsNull()
+        {
+            // Act
+            Utilisateur user = _jwtAuthentificationService.Authenticate("invalid@gmail.com", "password");
+
+            // Assert
+            Assert.IsNull(user);
+        }
+
+        [TestMethod]
+        public void GenerateToken_ValidClaims_ReturnsToken()
+        {
+            // Arrange
+            string secret = "mysecret";
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, "admin@gmail.com"),
+                new Claim(ClaimTypes.Role, "admin")
+            };
+
+            // Act
+            string token = _jwtAuthentificationService.GenerateToken(secret, claims);
+
+            // Assert
+            Assert.IsNotNull(token);
+            Assert.IsFalse(string.IsNullOrEmpty(token));
+        }
+    }
+}
